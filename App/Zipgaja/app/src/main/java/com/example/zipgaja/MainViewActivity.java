90e9c06470ad70bfbd4570d672cd4e11860a80d9
Kim Ahyun <kimahyunn132@gmail.com>
2022-05-12 21:16:35 +0900

    [FEAT] 버튼 추가 및 오류 수정

--- App/Zipgaja/app/src/main/java/com/example/zipgaja/MainViewActivity.java
+++ App/Zipgaja/app/src/main/java/com/example/zipgaja/MainViewActivity.java
@@ -2,6 +2,7 @@ package com.example.zipgaja;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -11,24 +12,32 @@ import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class MainViewActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     boolean alarm = false;
+    private GoogleMap mMap;
 
     String[] permission_list = {
             Manifest.permission.ACCESS_FINE_LOCATION,
@@ -42,6 +51,11 @@ public class MainViewActivity extends AppCompatActivity implements OnMapReadyCal
 
         checkPermission();
 
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+
+        // 현재 위치의 위도/경도 불러오기
         ImageButton currentBtn = findViewById(R.id.currentBtn);
         currentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,14 +65,43 @@ public class MainViewActivity extends AppCompatActivity implements OnMapReadyCal
             }
         });
 
+        // 현재 위치, 목적지 입력하기
+        EditText inputCurrent = findViewById(R.id.inputCurrent);
+        EditText inputDestination = findViewById(R.id.inputDestination);
 
+        // 엔터 입력 시 키보드 내리기
+        inputCurrent.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(inputCurrent.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+        inputDestination.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(inputDestination.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // SearchListActivity 로 Activity 전환
         ImageButton routeSearchBtn = (ImageButton) findViewById(R.id.routeSearchBtn);
-
         routeSearchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), SearchListActivity.class);
-                startActivity(i);
+                Intent intent = new Intent(getApplicationContext(), SearchListActivity.class);
+                intent.putExtra("currentLocation", inputCurrent.getText().toString());
+                intent.putExtra("destinationLocation", inputDestination.getText().toString());
+                startActivity(intent);
                 finish();
             }
         });
@@ -79,7 +122,6 @@ public class MainViewActivity extends AppCompatActivity implements OnMapReadyCal
                 }
             }
         });
-
     }
 
     private void checkPermission() {
@@ -105,10 +147,10 @@ public class MainViewActivity extends AppCompatActivity implements OnMapReadyCal
             for (int i = 0;i < grantResults.length; i++) {
                 // 허용됐다면
                 if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-
+                    Toast.makeText(getApplicationContext(), "앱 권한 설정 허용", Toast.LENGTH_LONG).show();
                 } else {
-                    Toast.makeText(getApplicationContext(), "앱 권한 설정 하세요", Toast.LENGTH_LONG).show();
-                    finish();
+                    Toast.makeText(getApplicationContext(), "앱 권한 설정 거부\n해당 앱 이용에 제한이 생길 수 있습니다.", Toast.LENGTH_LONG).show();
+                    // finish();
                 }
             }
         }
@@ -124,6 +166,7 @@ public class MainViewActivity extends AppCompatActivity implements OnMapReadyCal
         long minTime = 10000;
         float minDistance = 0;
 
+        // minTime 마다 location update
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                 minTime,
                 minDistance,
@@ -133,11 +176,16 @@ public class MainViewActivity extends AppCompatActivity implements OnMapReadyCal
 
     private class GPSListener implements LocationListener {
         public void onLocationChanged(Location location) {
+            // location 이 null 일 경우
+            if (location == null) {
+                return;
+            }
+
             // capture location data sent by current provider
             double latitude = location.getLatitude();
             double longitude = location.getLongitude();
 
-            String msg = "\nLatitude : " + latitude + "\nLongitude : " + longitude;
+            String msg = "Latitude : " + latitude + "\nLongitude : " + longitude;
             Log.i("GPSLocationService", msg);
             Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
         }
@@ -148,36 +196,25 @@ public class MainViewActivity extends AppCompatActivity implements OnMapReadyCal
     }
 
     // Get a handle to the GoogleMap object and display marker.
+//    @Override
+//    public void onMapReady(@NonNull GoogleMap googleMap) {
+//        googleMap.addMarker(new MarkerOptions()
+//                .position(new LatLng(0, 0))
+//                .title("Marker"));
+//    }
+
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
+    public void onMapReady(@NonNull final GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        mMap.addMarker(markerOptions);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
     }
 
 }
-
-
-//    Location lastKnownLocation = null;
-//    LocationListener locationListener = new LocationListener() {
-//
-//        @Override
-//        public void onLocationChanged(Location location) {
-//            LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-//
-//            // Get the last location
-//            lastKnownLocation = location;
-//
-//            // Stop the updates as soon as get the location
-//            // locationManager.removeUpdates(locationListener);
-//
-//            double latitude = location.getLatitude();
-//            double longitude = location.getLongitude();
-//        }
-//        @Override
-//        public void onProviderDisabled(String provider) { }
-//        @Override
-//        public void onProviderEnabled(String provider) { }
-//        @Override
-//        public void onStatusChanged(String provider, int status, Bundle extras) { }
-//    };
