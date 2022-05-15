dd6a5838177036416721e40704bb60a0bd0ab16a
inrir <kimms5617@naver.com>
2022-05-15 16:02:30 +0900

    [FEAT] 추천 비추천 데이터 받는 API 구현 #12

---
+++ Server/module/db_connect.js
@@ -0,0 +1,22 @@
+// database 연결 구조
+const db = require("mysql");
+require("dotenv").config(); // .env 하기 위한 dotenv 라이브러리 
+
+module.exports.conn = function(){
+    const conn = db.createConnection({
+        host: process.env.host,
+        user: process.env.user,
+        password: process.env.password,
+        database: process.env.database
+    });
+    conn.connect(function(err){
+        if(err){
+            console.error('에러 connect:' + err.stack);
+            return;
+        }
+        console.log("Mysql DB Connect완료! ID : " + conn.threadId);
+    });
+    return conn;
+}
+
+
