e85ae4b05e3a1589e77cc66dbda36293531890d2
parkikbum <pibum@naver.com>
2022-05-31 02:52:26 +0900

    [CHORE] 모든 버스정류장이 아닌 심야버스 정류장만 불러올 수 있도록 수정

--- Server/routes/Api/Main/getStation.js
+++ Server/routes/Api/Main/getStation.js
@@ -1,37 +1,28 @@
 const request = require('request')
-const bike = require('./bikeStation');
+const {bikeStationArr} = require('./bikeStation');
+const {bus_location} = require("../Map/nightBusData");
 const busServiceKey = 't5N%2FFqNynkqx15wlqztjL4KgGPu4RnntvpSnvcC2Jx6Czvtphqeg8sXDPp%2BdMI48sTmABuKEDErpcqzM8dRZxw%3D%3D'
 
 //공공데이터 포털에서 받은 내 서비스키 
 var parse = require('json-parse');
 
+const getStartBusData = (latitude, longitude, distance) => {
+    //내 출발지 좌표로 부터의 주변 버스정류장
+    const startPointBusStation = bus_location.filter(data => data.longitude <= (longitude + distance) && data.longitude >= (longitude - distance) && data.latitude <= (latitude + distance) && data.latitude >= (latitude - distance));
+    return startPointBusStation;
+}
 //따릉이 스테이션 정보 불러오기
-const getStation = (userLatitude, userLongitude, callback) => {      
-    const bikeData = bike.bikeStationArr.filter(data => data.longitude <= (userLongitude + 0.005) && data.longitude >= (userLongitude - 0.005) && data.latitude <= (userLatitude + 0.005) && data.latitude >= (userLatitude - 0.005));
+const getStation = (userLatitude, userLongitude, callback) => {     
+    const distance = 0.005; 
+    var bikeData = bikeStationArr.filter(data => data.longitude <= (userLongitude + distance) && data.longitude >= (userLongitude - distance) && data.latitude <= (userLatitude + distance) && data.latitude >= (userLatitude - distance));
     var busData = JSON;
     var busDesc = [];
 
-    var url = 'http://ws.bus.go.kr/api/rest/stationinfo/getStationByPos';
-    var queryParams = '?serviceKey=' + busServiceKey ; /* Service Key*/
-    queryParams += '&tmX=' + encodeURIComponent(userLongitude); /* */
-    queryParams += '&tmY=' + encodeURIComponent(userLatitude); /* */
-    queryParams += '&radius=' + encodeURIComponent(500); /* */
-    queryParams += '&resultType=' + encodeURIComponent('json'); /* */
-
-    request({
-    url: url + queryParams,
-    method: 'GET'
-    }, function (error, response, body) {
-    // console.log(url + queryParams)
-    console.log('Status', response.statusCode);
-    // console.log('Headers', JSON.stringify(response.headers));
-    busData = JSON.parse(body);
-    busDesc = busData.msgBody.itemList;    
+    busDesc = getStartBusData(userLatitude, userLongitude, 0.005);
 
     callback(undefined,{   
         bikeStation: bikeData,
         busStation: busDesc
-    });
     });
         
 }
