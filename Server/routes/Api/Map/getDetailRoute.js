4830d3690e46e7d93850f209cb3b7fe54638adbb
parkikbum <pibum@naver.com>
2022-06-02 02:06:44 +0900

    [FEAT] 상세 경로 조회 기능 구현 완료

--- Server/routes/Api/Map/getDetailRoute.js
+++ Server/routes/Api/Map/getDetailRoute.js
@@ -38,31 +38,54 @@ async function main(routeId, start, end, callback) {
 
         if (startBusStation - endBusStation > 0) {
             var busStation = bus_location.filter(data => (data.station_num <= route.bus_start) && (data.station_num >= route.bus_end) && data.name == route.bus_num);
-            var reversBus = busStation.reverse().map(data => data);
-            console.log(busStation);
+            var reversBus = busStation.slice(0).reverse().map(data => data);
+            console.log(reversBus);
             //따릉이 포함 경로일때 
             if (route.s_bike_long != 0) {
-                connectOSRM(route.s_bike_long, route.s_bike_lati, busStation[0].longitude, busStation[0].latitude);
+                connectOSRM(route.start_long, route.start_lati, route.s_bike_long, route.s_bike_lati);
+                connectOSRM(route.s_bike_long, route.s_bike_lati, reversBus[0].longitude, reversBus[0].latitude);
                 
-                busStation.map((busStation, index) => connectOSRM(reversBus[index].longitude, reversBus[index].latitude, reversBus[index + 1 >= reversBus.length ? index : index + 1].longitude, reversBus[index + 1 >= reversBus.length ? index : index + 1].latitude));
+                reversBus.map((busStation, index) => connectOSRM(reversBus[index].longitude, reversBus[index].latitude, reversBus[index + 1 > reversBus.length - 1 ? index : index + 1].longitude, reversBus[index + 1 > reversBus.length - 1 ? index : index + 1].latitude));
                 const returnRoute = encodePolyline()
+
+                connectOSRM(route.fs_bike_long, route.fs_bike_lati, route.fe_bike_long, route.fe_bike_lati);
+                connectOSRM(route.fe_bike_long, route.fe_bike_lati, route.end_long, route.end_lati);
+
                 
                 returnData.push({route: returnRoute});
 
                 //따릉이
-                returnData.push({name: start + "~" + bikeStationArr.filter(data => data.longitude == route.s_bike_long && data.latitude == route.s_bike_lati).name, time: calcWalkingTime(getDistance(route.s_bike_lati, route.s_bike_long, route.start_lati, route.start_long)), cost: 0, type: "walk"});
-                returnData.push({name: bikeStationArr.filter(data => data.longitude == route.s_bike_long && data.latitude == route.s_bike_lati).name + "~" + bikeStationArr.filter(data => data.longitude == route.e_bike_long && data.latitude == route.e_bike_lati).name, time: calcuBike(route.s_bike_long, route.s_bike_lati, route.e_bike_long, route.e_bike_lati), cost: 1000, type: "bike"});
-                returnData.push({name: bikeStationArr.filter(data => data.longitude == route.e_bike_long && data.latitude == route.e_bike_lati).name + "~" + reversBus[0].name, time: calcWalkingTime(getDistance(route.e_bike_lati, route.e_bike_long, reversBus[0].latitude, reversBus[0].long)), cost: 0, type: "walk"});
+                returnData.push({name: start + "~" + bikeStationArr.filter(data => data.longitude == route.s_bike_long && data.latitude == route.s_bike_lati)[0].name + "(따릉이)", time: calcWalkingTime(getDistance(route.s_bike_lati, route.s_bike_long, route.start_lati, route.start_long)), cost: 0, type: "walk"});
+                returnData.push({name: bikeStationArr.filter(data => data.longitude == route.s_bike_long && data.latitude == route.s_bike_lati)[0].name + " ~ " + bikeStationArr.filter(data => data.longitude == route.e_bike_long && data.latitude == route.e_bike_lati)[0].name, time: calcuBike(route.s_bike_long, route.s_bike_lati, route.e_bike_long, route.e_bike_lati), cost: 1000, type: "bike"});
+                returnData.push({name: bikeStationArr.filter(data => data.longitude == route.e_bike_long && data.latitude == route.e_bike_lati)[0].name + " ~ " + reversBus[0].station_name + "("+reversBus[0].name+")", time: calcWalkingTime(getDistance(route.e_bike_lati, route.e_bike_long, reversBus[0].latitude, reversBus[0].longitude)), cost: 0, type: "walk"});
 
                 //버스
                 returnData.push({name: reversBus[0].station_name + " ~ " + busStation[0].station_name, time: calcBusTime(busStation.length), cost: 2150, type: "bus"});
 
                 //따릉이
-                returnData.push({name: busStation[0].station_name + "~" + bikeStationArr.filter(data => data.longitude == route.fs_bike_long && data.latitude == route.fs_bike_lati).name, time: calcWalkingTime(getDistance(route.fs_bike_lati, route.fs_bike_long, busStation[0].latitude, busStation[0].start_long)), cost: 0, type: "walk"});
-                returnData.push({name: bikeStationArr.filter(data => data.longitude == route.fs_bike_long && data.latitude == route.fs_bike_lati).name + "~" + bikeStationArr.filter(data => data.longitude == route.fe_bike_long && data.latitude == route.fe_bike_lati).name, time: calcuBike(route.fs_bike_long, route.fs_bike_lati, route.fe_bike_long, route.fe_bike_lati), cost: 1000, type: "bike"});
-                returnData.push({name: bikeStationArr.filter(data => data.longitude == route.e_bike_long && data.latitude == route.e_bike_lati).name + "~" + end, time: calcWalkingTime(getDistance(route.e_bike_lati, route.e_bike_long, route.end_lati, route.end_long)), cost: 0, type: "walk"});
+                returnData.push({name: busStation[0].station_name + "~" + bikeStationArr.filter(data => data.longitude == route.fs_bike_long && data.latitude == route.fs_bike_lati)[0].name + "(따릉이)", time: calcWalkingTime(getDistance(route.fs_bike_lati, route.fs_bike_long, busStation[0].latitude, busStation[0].longitude)), cost: 0, type: "walk"});
+                returnData.push({name: bikeStationArr.filter(data => data.longitude == route.fs_bike_long && data.latitude == route.fs_bike_lati)[0].name + " ~ " + bikeStationArr.filter(data => data.longitude == route.fe_bike_long && data.latitude == route.fe_bike_lati)[0].name, time: calcuBike(route.fs_bike_long, route.fs_bike_lati, route.fe_bike_long, route.fe_bike_lati), cost: 1000, type: "bike"});
+                returnData.push({name: bikeStationArr.filter(data => data.longitude == route.fe_bike_long && data.latitude == route.fe_bike_lati)[0].name + "(따릉이) ~ " + end, time: calcWalkingTime(getDistance(route.fe_bike_lati, route.fe_bike_long, route.end_lati, route.end_long)), cost: 0, type: "walk"});
 
-                returnData.push({name: end, time: 0, cost: 0, type: "walk"});
+            }
+            else {
+                //도보로 정류장 이동
+                connectOSRM(route.start_long, route.start_lati, reversBus[0].longitude, reversBus[0].latitude);
+                //버스
+                reversBus.map((busStation, index) => connectOSRM(reversBus[index].longitude, reversBus[index].latitude, reversBus[index + 1 > reversBus.length - 1 ? index : index + 1].longitude, reversBus[index + 1 > reversBus.length - 1 ? index : index + 1].latitude));
+                //도착
+                connectOSRM(reversBus[reversBus.length - 1].longitude, reversBus[reversBus.length - 1].latitude, route.end_long, route.end_lati);
+                
+                const returnRoute = encodePolyline()
+                returnData.push({route: returnRoute});
+
+                //도보로 정류장
+                returnData.push({name: start + "~" + reversBus[0].station_name, time: calcWalkingTime(getDistance(route.start_lati, route.start_long, reversBus[0].latitude, reversBus[0].longitude)), cost: 0, type: "walk"});
+                //버스 노선
+                returnData.push({name: reversBus[0].station_name + "(" + reversBus[0].name + ")" + "~" + reversBus[reversBus.length - 1].station_name, time: calcBusTime(reversBus.length), cost: 2150, type: "bus"});
+                //도보로 목적지
+                returnData.push({name: reversBus[reversBus.length - 1].station_name + "~" + end, time: calcWalkingTime(getDistance(reversBus[reversBus.length - 1].latitude, reversBus[reversBus.length - 1].longitude, route.end_lati, route.end_long)), cost: 0, type: "walk"});
+
             }
         }
         else if (startBusStation - endBusStation < 0) {
@@ -74,9 +97,42 @@ async function main(routeId, start, end, callback) {
                 
                 busStation.map((busStation, index) => connectOSRM(resultbust[index].longitude, resultbust[index].latitude, resultbust[index + 1 >= resultbust.length ? index : index + 1].longitude, resultbust[index + 1 >= resultbust.length ? index : index + 1].latitude));
                 const returnRoute = encodePolyline()
-                console.log(returnRoute);
+                connectOSRM(route.fs_bike_long, route.fs_bike_lati, route.fe_bike_long, route.fe_bike_lati);
+                connectOSRM(route.fe_bike_long, route.fe_bike_lati, route.end_long, route.end_lati);
+
+                
+                returnData.push({route: returnRoute});
+
+                //따릉이
+                returnData.push({name: start + "~" + bikeStationArr.filter(data => data.longitude == route.s_bike_long && data.latitude == route.s_bike_lati)[0].name + "(따릉이)", time: calcWalkingTime(getDistance(route.s_bike_lati, route.s_bike_long, route.start_lati, route.start_long)), cost: 0, type: "walk"});
+                returnData.push({name: bikeStationArr.filter(data => data.longitude == route.s_bike_long && data.latitude == route.s_bike_lati)[0].name + " ~ " + bikeStationArr.filter(data => data.longitude == route.e_bike_long && data.latitude == route.e_bike_lati)[0].name, time: calcuBike(route.s_bike_long, route.s_bike_lati, route.e_bike_long, route.e_bike_lati), cost: 1000, type: "bike"});
+                returnData.push({name: bikeStationArr.filter(data => data.longitude == route.e_bike_long && data.latitude == route.e_bike_lati)[0].name + " ~ " + busStation[0].station_name + "("+busStation[0].name+")", time: calcWalkingTime(getDistance(route.e_bike_lati, route.e_bike_long, busStation[0].latitude, busStation[0].longitude)), cost: 0, type: "walk"});
+
+                //버스
+                returnData.push({name: busStation[0].station_name + " ~ " + busStation[busStation.length - 1].station_name, time: calcBusTime(busStation.length), cost: 2150, type: "bus"});
+
+                //따릉이
+                returnData.push({name: busStation[busStation.length - 1].station_name + "~" + bikeStationArr.filter(data => data.longitude == route.fs_bike_long && data.latitude == route.fs_bike_lati)[0].name + "(따릉이)", time: calcWalkingTime(getDistance(route.fs_bike_lati, route.fs_bike_long, busStation[busStation.length - 1].latitude, busStation[busStation.length - 1].longitude)), cost: 0, type: "walk"});
+                returnData.push({name: bikeStationArr.filter(data => data.longitude == route.fs_bike_long && data.latitude == route.fs_bike_lati)[0].name + " ~ " + bikeStationArr.filter(data => data.longitude == route.fe_bike_long && data.latitude == route.fe_bike_lati)[0].name, time: calcuBike(route.fs_bike_long, route.fs_bike_lati, route.fe_bike_long, route.fe_bike_lati), cost: 1000, type: "bike"});
+                returnData.push({name: bikeStationArr.filter(data => data.longitude == route.fe_bike_long && data.latitude == route.fe_bike_lati)[0].name + "(따릉이) ~ " + end, time: calcWalkingTime(getDistance(route.fe_bike_lati, route.fe_bike_long, route.end_lati, route.end_long)), cost: 0, type: "walk"});
             }
             else {
+                //도보로 정류장 이동
+                connectOSRM(route.start_long, route.start_lati, busStation[0].longitude, busStation[0].latitude);
+                //버스
+                busStation.map((busStation, index) => connectOSRM(resultbust[index].longitude, resultbust[index].latitude, resultbust[index + 1 > resultbust.length - 1 ? index : index + 1].longitude, resultbust[index + 1 > resultbust.length - 1 ? index : index + 1].latitude));
+                //도착
+                connectOSRM(busStation[busStation.length - 1].longitude, busStation[busStation.length - 1].latitude, route.end_long, route.end_lati);
+                
+                const returnRoute = encodePolyline()
+                returnData.push({route: returnRoute});
+
+                //도보로 정류장
+                returnData.push({name: start + "~" + busStation[0].station_name, time: calcWalkingTime(getDistance(route.start_lati, route.start_long, busStation[0].latitude, busStation[0].longitude)), cost: 0, type: "walk"});
+                //버스 노선
+                returnData.push({name: busStation[0].station_name + "(" + busStation[0].name + ")" + "~" + busStation[busStation.length - 1].station_name, time: calcBusTime(busStation.length), cost: 2150, type: "bus"});
+                //도보로 목적지
+                returnData.push({name: busStation[busStation.length - 1].station_name + "~" + end, time: calcWalkingTime(getDistance(busStation[busStation.length - 1].latitude, busStation[busStation.length - 1].longitude, route.end_lati, route.end_long)), cost: 0, type: "walk"});
 
             }
         }
@@ -97,22 +153,6 @@ function sleep(ms) {
   }
 
 
-//osrm과 연동해서 경로 데이터 불러오기
-async function connectOSRM (req) {
-    // sLong, sLati, eLong, eLati
-    var url = 'http://3.82.223.178/route/v1/driving/';
-    // var queryParams =  sLong + "," + sLati + ";" + eLong + "," + eLati;
-    var queryParams = req[0].longitude + "," + req[0].latitude + ";" + req[1].longitude + "," + req[1].latitude;
-    
-    var route = s_request('GET', url + queryParams);
-    var jsonRoute = JSON.parse(route.getBody('utf8'));
-    routeData.push(polyline.decode(jsonRoute.routes[0].geometry));
-    
-    distanceData.push(jsonRoute.routes[0].distance);
-
-    return;
-}
-
 async function connectOSRM (sLong, sLati, eLong, eLati) {
     // sLong, sLati, eLong, eLati
     var url = 'http://3.82.223.178/route/v1/driving/';
@@ -122,6 +162,7 @@ async function connectOSRM (sLong, sLati, eLong, eLati) {
     var route = s_request('GET', url + queryParams);
     var jsonRoute = JSON.parse(route.getBody('utf8'));
     routeData.push(polyline.decode(jsonRoute.routes[0].geometry));
+
     
     distanceData.push(jsonRoute.routes[0].distance);
 
