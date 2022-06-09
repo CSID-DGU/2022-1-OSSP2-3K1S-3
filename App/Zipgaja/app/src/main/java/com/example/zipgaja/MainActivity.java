b066b5727845577d769549d437bdf5accf0fc132
Kim Ahyun <kimahyunn132@gmail.com>
2022-06-09 17:35:03 +0900

    [ADD] View Merge

--- App/Zipgaja/app/src/main/java/com/example/zipgaja/MainActivity.java
+++ App/Zipgaja/app/src/main/java/com/example/zipgaja/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.zipgaja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
